b7d5c7085d2e7a6ac11aad1058656bfca0975086
Yang <r9young@hotmail.com>
2024-06-05 21:09:20 +1000

    put it back

--- index.js
+++ index.js
@@ -1,51 +1,3 @@
-// import express from 'express';
-// import bodyParser from 'body-parser';
-
-// import db from "./mongoC.js";
-
-// const port = 4000;
-// const app = express();
-
-// app.use((_req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', '*');
-  
-//     next();
-//   });
-
-// // Parses the text as url encoded data
-// app.use(bodyParser.urlencoded({ extended: true }));
- 
-// // Parses the text as json
-// app.use(bodyParser.json());
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World, from express');
-// })
-
-// app.post('/addUser',async (req, res) => {
-//     let collection = await db.collection("users");
-//     let newDocument = req.body;
-//     newDocument.date = new Date();
-//     let result = await collection.insertOne(newDocument);
-//     // console.log("rreq"+req.body);
-//     console.log("Request body: ", req.body);
-//     res.send(result).status(204);
-// });
-
-// app.get('/getUsers', async(req, res) => {
-//     let collection = await db.collection("users");
-//     let results = await collection.find({})
-      
-//       .toArray();
-//     res.send(results).status(200);
-// });
-
-// app.listen(port, function () {
-//     console.log("Server is listening at port:" + port);
-// });
-
-
 import express from 'express';
 import bodyParser from 'body-parser';
 
@@ -57,34 +9,39 @@ const app = express();
 app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', '*');
+  
     next();
-});
+  });
 
-// Parses the text as URL-encoded data
+// Parses the text as url encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Parses the text as JSON
+ 
+// Parses the text as json
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
-});
+})
 
-app.post('/addUser', async (req, res) => {
-    let collection = db.collection("users"); // Correctly accessing the 'users' collection
+app.post('/addUser',async (req, res) => {
+    let collection = await db.collection("users");
     let newDocument = req.body;
     newDocument.date = new Date();
     let result = await collection.insertOne(newDocument);
+    // console.log("rreq"+req.body);
     console.log("Request body: ", req.body);
-    res.status(204).send(result);
+    res.send(result).status(204);
 });
 
-app.get('/getUsers', async (req, res) => {
-    let collection = db.collection("users"); // Correctly accessing the 'users' collection
-    let results = await collection.find({}).toArray();
-    res.status(200).send(results);
+app.get('/getUsers', async(req, res) => {
+    let collection = await db.collection("users");
+    let results = await collection.find({})
+      
+      .toArray();
+    res.send(results).status(200);
 });
 
 app.listen(port, function () {
     console.log("Server is listening at port:" + port);
 });
+
