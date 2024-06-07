a193ebab94c0349ef3f415591bf67601567d8171
Yang <r9young@hotmail.com>
2024-06-07 21:28:36 +1000

    test

--- backup/mongoC.js
+++ backup/mongoC.js
@@ -1,8 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString =`mongodb+srv://r9young777:${password}@devcluster.cxbhzdm.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`
-
+const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
@@ -11,5 +10,5 @@ try {
 } catch(e) {
   console.error(e);
 }
-let db = conn.db("r9young777");
+let db = conn.db("integration_ninjas");
 export default db;
