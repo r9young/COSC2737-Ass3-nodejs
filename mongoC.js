a67d4e696757bef79bd04f41388adfde57923de3
Yang <r9young@hotmail.com>
2024-06-07 13:49:54 +1000

    make back

--- mongoC.js
+++ mongoC.js
@@ -1,35 +1,17 @@
-// import { MongoClient } from "mongodb";
-
-// const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-// // const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
-// const connectionString =`mongodb+srv://r9young777:${password}@devcluster.cxbhzdm.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`
-
-// const client = new MongoClient(connectionString);
-// let conn;
-// try {
-//   conn = await client.connect();
-//   console.log("connection successful")
-// } catch(e) {
-//   console.error(e);
-// }
-// let db = conn.db("r9young777");
-// export default db;
-
 import { MongoClient } from "mongodb";
-import 'dotenv/config';
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
 // const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
-const connectionString = `mongodb+srv://r9young777:${password}@devcluster.cxbhzdm.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`;
+const connectionString =`mongodb+srv://r9young777:${password}@devcluster.cxbhzdm.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`
 
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful");
-} catch (e) {
+  console.log("connection successful")
+} catch(e) {
   console.error(e);
 }
-
-let db = conn ? conn.db("r9young777") : null;
+let db = conn.db("r9young777");
 export default db;
+
