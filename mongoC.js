b380aedc5a580f3ffbb5e4c17b97d892431751fa
Yang <r9young@hotmail.com>
2024-06-07 21:09:59 +1000

    back

--- mongoC.js
+++ mongoC.js
@@ -1,14 +1,14 @@
-
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://r9young777:${password}@devcluster.cxbhzdm.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`;
+// const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString =`mongodb+srv://r9young777:${password}@devcluster.cxbhzdm.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`
 
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
-  console.log("connection successful");
+  console.log("connection successful")
 } catch(e) {
   console.error(e);
 }
