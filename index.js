5daa8dbe760cfd4583bfb95f387ba15d97d7a2c4
Yang <r9young@hotmail.com>
2024-06-07 14:04:20 +1000

    back

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-
 import db from "./mongoC.js";
 
 const port = 4000;
@@ -29,72 +28,18 @@ app.post('/addUser',async (req, res) => {
     let newDocument = req.body;
     newDocument.date = new Date();
     let result = await collection.insertOne(newDocument);
-    // console.log("rreq"+req.body);
-    console.log("Request body: ", req.body);
+    console.log("rreq"+req.body);
     res.send(result).status(204);
 });
 
-app.get('/getUsers', async (req, res) => {
-    try {
-        let collection = await db.collection("users");
-        let results = await collection.find({}).toArray();
-        res.status(200).send(results);
-    } catch (e) {
-        console.error(e);
-        res.status(500).send('Error retrieving users');
-    }
+app.get('/getUsers', async(req, res) => {
+    let collection = await db.collection("users");
+    let results = await collection.find({})
+      
+      .toArray();
+    res.send(results).status(200);
 });
 
 app.listen(port, function () {
     console.log("Server is listening at port:" + port);
 });
-
-// import express from 'express';
-// import bodyParser from 'body-parser';
-// import cors from 'cors';
-// import db from "./mongoC.js";
-
-// const port = 4000;
-// const app = express();
-
-// // Use the cors middleware
-// app.use(cors());
-
-// // Parses the text as url encoded data
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // Parses the text as json
-// app.use(bodyParser.json());
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World, from express');
-// });
-
-// app.post('/addUser', async (req, res) => {
-//     try {
-//         let collection = await db.collection("users");
-//         let newDocument = req.body;
-//         newDocument.date = new Date();
-//         let result = await collection.insertOne(newDocument);
-//         console.log("Request body: ", req.body);
-//         res.status(201).send(result);
-//     } catch (e) {
-//         console.error(e);
-//         res.status(500).send('Error adding user');
-//     }
-// });
-
-// app.get('/getUsers', async (req, res) => {
-//     try {
-//         let collection = await db.collection("users");
-//         let results = await collection.find({}).toArray();
-//         res.status(200).send(results);
-//     } catch (e) {
-//         console.error(e);
-//         res.status(500).send('Error retrieving users');
-//     }
-// });
-
-// app.listen(port, function () {
-//     console.log("Server is listening at port:" + port);
-// });
