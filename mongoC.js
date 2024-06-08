963465ae53a5a2349373bd8d43015eaccdac0ef3
Yang <r9young@hotmail.com>
2024-06-08 20:42:52 +1000

    try again

--- mongoC.js
+++ mongoC.js
@@ -1,1 +1,15 @@
-C
+import { MongoClient } from "mongodb";
+
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+const connectionString = `mongodb+srv://r9young777:${password}@devcluster.cxbhzdm.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`;
+
+const client = new MongoClient(connectionString);
+let conn;
+try {
+  conn = await client.connect();
+  console.log("connection successful");
+} catch(e) {
+  console.error(e);
+}
+let db = conn.db("r9young777");
+export default db;
