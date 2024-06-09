93e9e832ca19560eed88067fabc78c4de3d2a3b7
Yang <r9young@hotmail.com>
2024-06-09 12:20:24 +1000

    delete dev

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors'; // Import the cors middleware
-import db from "./mongoC.js"; // Import the database connection
+import cors from 'cors';
+import connectDB from './mongoC.js';
+import speakeasy from 'speakeasy';
+import qrcode from 'qrcode';
 
 const port = 4000;
 const app = express();
 
-// Use cors middleware to enable CORS with various options
 app.use(cors());
-
-// Middleware to parse JSON and URL encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+let db;
+
+connectDB().then(database => {
+  db = database;
+  app.listen(port, () => {
+    console.log("Server is listening at port:" + port);
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World, from express');
 });
 
-app.post('/addUser', async (req, res) => { // Added leading slash
+app.post('/addUser', async (req, res) => {
   try {
     let collection = await db.collection("users");
     let newDocument = req.body;
@@ -31,7 +39,7 @@ app.post('/addUser', async (req, res) => { // Added leading slash
   }
 });
 
-app.get('/getUser', async (req, res) => { // Added leading slash
+app.get('/getUser', async (req, res) => {
   try {
     let collection = await db.collection("users");
     let results = await collection.find({}).toArray();
@@ -42,12 +50,6 @@ app.get('/getUser', async (req, res) => { // Added leading slash
   }
 });
 
-app.listen(port, () => {
-  console.log("Server is listening at port:" + port);
-});
-
-
-// Add the /api/login endpoint
 app.post('/api/login', async (req, res) => {
   const { username, password } = req.body;
 
@@ -55,8 +57,8 @@ app.post('/api/login', async (req, res) => {
     let collection = await db.collection("users");
     let user = await collection.findOne({ username });
 
-    if (user && user.password === password) { // Simplified authentication
-      res.status(200).send({ success: true });
+    if (user && user.password === password) {
+      res.status(200).send({ success: true, userId: user._id });
     } else {
       res.status(401).send({ success: false });
     }
@@ -64,4 +66,26 @@ app.post('/api/login', async (req, res) => {
     console.error('Error:', error);
     res.status(500).send('An error occurred');
   }
+});
+
+app.post('/enable-mfa', async (req, res) => {
+  const { userId } = req.body;
+
+  const secret = speakeasy.generateSecret({ length: 20 });
+
+  await db.collection('users').updateOne({ _id: userId }, { $set: { mfaSecret: secret.base32 } });
+
+  const qrCodeUrl = speakeasy.otpauthURL({
+    secret: secret.ascii,
+    label: 'YourAppName',
+    issuer: 'YourAppName',
+    encoding: 'base32'
+  });
+
+  qrcode.toDataURL(qrCodeUrl, (err, dataUrl) => {
+    if (err) {
+      return res.status(500).json({ error: 'Failed to generate QR code' });
+    }
+    res.json({ qrCodeUrl: dataUrl });
+  });
 });
