5a6e99bad44eb0b782b8751c996a2499b82e496c
Yang <r9young@hotmail.com>
2024-06-10 11:10:09 +1000

    update login

--- index.js
+++ index.js
@@ -55,15 +55,9 @@ app.post('/api/login', async (req, res) => {
     if (user && user.password === password) {
       const response = {
         success: true,
-        userId: user._id.toString()
+        userId: user._id.toString(),
+        mfaSecret: user.mfaEnabled ? user.mfaSecret : null
       };
-
-      // Check if MFA is enabled for the user
-      if (user.mfaEnabled) {
-        response.mfaSecret = user.mfaSecret;
-      }
-      
-
       res.status(200).send(response);
     } else {
       res.status(401).send({ success: false });
